f3d147172e3a4f72b7043d8581d186838447256b
yozo Gregson <yozolarlund@gmail.com>
2026-01-22 10:54:56 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3403788432-80355",
+  "appId": "1:1091087477007:web:b4f60497dbb989889cf67a",
+  "apiKey": "AIzaSyBw6jUIIq_CFF0sXiu6VO67sxX-dyEI5Vw",
+  "authDomain": "studio-3403788432-80355.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1091087477007"
+};

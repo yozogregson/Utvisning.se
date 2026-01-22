3508e170fd1a1eb3fd1d871524817494a86d0b6b
yozo Gregson <yozolarlund@gmail.com>
2026-01-22 12:59:21 +0000

    ## Error Type Console FirebaseError
    ## Error Message
    Firebase Storage:

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:1091087477007:web:b4f60497dbb989889cf67a",
   "apiKey": "AIzaSyBw6jUIIq_CFF0sXiu6VO67sxX-dyEI5Vw",
   "authDomain": "studio-3403788432-80355.firebaseapp.com",
+  "storageBucket": "studio-3403788432-80355.appspot.com",
   "measurementId": "",
   "messagingSenderId": "1091087477007"
 };

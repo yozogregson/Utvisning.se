8bf087f6865595977ba131a458260cccd1d75851
yozo Gregson <yozolarlund@gmail.com>
2026-01-24 14:20:22 +0000

    <iframe data-tally-src="https://tally.so/embed/dW9Zgd?alignLeft=1&hideTi

--- src/components/analysis-form.tsx
+++ src/components/analysis-form.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 export function AnalysisForm() {
   return (
     <iframe
-      data-tally-src="https://tally.so/embed/dW9Zgd?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
+      data-tally-src="https://tally.so/embed/dW9Zgd?alignLeft=1&hideTitle=1&transparentBackground=1"
       loading="lazy"
       width="100%"
       height="285"

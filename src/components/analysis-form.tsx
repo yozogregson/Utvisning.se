9df52f1445709ddbd5ad258573f15893a7edd73a
yozo Gregson <yozolarlund@gmail.com>
2026-01-24 14:45:38 +0000

    okej ändra height till 400

--- src/components/analysis-form.tsx
+++ src/components/analysis-form.tsx
@@ -8,7 +8,7 @@ export function AnalysisForm() {
       data-tally-src="https://tally.so/embed/dW9Zgd?alignLeft=1&hideTitle=1&transparentBackground=1"
       loading="lazy"
       width="80%"
-      height="50"
+      height="400"
       frameBorder="0"
       marginHeight="0"
       marginWidth="0"

4809422f0f7a82f939dfda3ebbfde782251541d0
yozo Gregson <yozolarlund@gmail.com>
2026-01-24 14:41:55 +0000

    <iframe data-tally-src="https://tally.so/embed/dW9Zgd?alignLeft=1&hideTi

--- src/components/analysis-form.tsx
+++ src/components/analysis-form.tsx
@@ -8,7 +8,7 @@ export function AnalysisForm() {
       data-tally-src="https://tally.so/embed/dW9Zgd?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
       loading="lazy"
       width="80%"
-      height="300"
+      height="200"
       frameBorder="0"
       marginHeight="0"
       marginWidth="0"

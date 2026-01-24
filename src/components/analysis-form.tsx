0d419d29328d6121108315cfbfa2b4e1f36045d1
yozo Gregson <yozolarlund@gmail.com>
2026-01-24 15:00:37 +0000

    <iframe data-tally-src="https://tally.so/embed/dW9Zgd?alignLeft=1&hideTi

--- src/components/analysis-form.tsx
+++ src/components/analysis-form.tsx
@@ -4,16 +4,15 @@ import React from 'react';
 
 export function AnalysisForm() {
   return (
-    <div style={{ width: '100%', maxWidth: '450px', margin: '0 auto' }}>
-      <iframe
-        src="https://tally.so/embed/dW9Zgd?hideTitle=1&transparentBackground=1"
-        width="100%"
-        height="700"
-        frameBorder="0"
-        scrolling="no"
-        style={{ border: 'none', overflow: 'hidden' }}
-        title="Starta din kostnadsfria analys"
-      ></iframe>
-    </div>
+    <iframe
+      data-tally-src="https://tally.so/embed/dW9Zgd?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
+      loading="lazy"
+      width="80%"
+      height="400"
+      frameBorder="0"
+      marginHeight="0"
+      marginWidth="0"
+      title="Starta din kostnadsfria analys"
+    ></iframe>
   );
 }

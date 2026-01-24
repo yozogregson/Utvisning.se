953c82ec6264ddedfe712afc57cb0242a1452403
yozo Gregson <yozolarlund@gmail.com>
2026-01-24 14:51:02 +0000

    sätt detta i body koden <div style="width: 100%; max-width: 450px; heigh

--- src/components/analysis-form.tsx
+++ src/components/analysis-form.tsx
@@ -4,15 +4,15 @@ import React from 'react';
 
 export function AnalysisForm() {
   return (
-    <iframe
-      data-tally-src="https://tally.so/embed/dW9Zgd?alignLeft=1&hideTitle=1&transparentBackground=1"
-      loading="lazy"
-      width="80%"
-      height="400"
-      frameBorder="0"
-      marginHeight="0"
-      marginWidth="0"
-      title="Starta din kostnadsfria analys"
-    ></iframe>
+    <div style={{ width: '100%', maxWidth: '450px', height: '400px', overflow: 'hidden', margin: '0 auto', border: 'none' }}>
+        <iframe 
+            src="https://tally.so/embed/dW9Zgd?hideTitle=1&alignLeft=1&transparentBackground=1" 
+            width="100%" 
+            height="100%" 
+            frameBorder="0" 
+            style={{ border: 'none', overflow: 'hidden' }}
+            title="Starta din kostnadsfria analys">
+        </iframe>
+    </div>
   );
 }

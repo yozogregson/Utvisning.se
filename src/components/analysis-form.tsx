6e5c72658acf7b893bc2662d779def1888a3aa36
yozo Gregson <yozolarlund@gmail.com>
2026-01-24 14:21:52 +0000

    jag vill inte den ska skrolla, fast jag vill inte ha den så stor som den

--- src/components/analysis-form.tsx
+++ src/components/analysis-form.tsx
@@ -8,7 +8,7 @@ export function AnalysisForm() {
       data-tally-src="https://tally.so/embed/dW9Zgd?alignLeft=1&hideTitle=1&transparentBackground=1"
       loading="lazy"
       width="100%"
-      height="285"
+      height="450"
       frameBorder="0"
       marginHeight="0"
       marginWidth="0"
